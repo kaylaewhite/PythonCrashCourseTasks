d287bc21091ec941f9f1c7d3ad8747ca5cce41b8
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-17 18:20:35 -0400

    exercises with list - no loops

---
+++ 9guestcount.py
@@ -0,0 +1,44 @@
+guest_list = ['Sarah', 'Albert', 'Nia', 'George']
+invite1 = f"Hello, {guest_list[0]}, you are invited to a dinner!"
+print(invite1)
+invite2 = f"Hello, {guest_list[1]}, you are invited to a dinner!"
+print(invite2)
+invite3 = f"Hello, {guest_list[2]}, you are invited to a dinner!"
+print(invite3)
+invite4 = f"Hello, {guest_list[3]}, you are invited to a dinner!"
+print(invite4)
+cancellation = f"As it turns out, {guest_list[1]} cannot attend."
+print(cancellation)
+guest_list.remove("Albert")
+print(guest_list)
+guest_list.insert(1, 'Harris')
+print(guest_list)
+invite1 = f"Hello, {guest_list[0]}, you are invited to a dinner!"
+print(invite1)
+invite2 = f"Hello, {guest_list[1]}, you are invited to a dinner!"
+print(invite2)
+invite3 = f"Hello, {guest_list[2]}, you are invited to a dinner!"
+print(invite3)
+invite4 = f"Hello, {guest_list[3]}, you are invited to a dinner!"
+print(invite4)
+expansion = "As it turns out, there will be more room at the dinner table."
+print(expansion)
+guest_list.insert(0, 'Nara')
+guest_list.insert(3, 'Steven')
+guest_list.append('Linda')
+invite1 = f"Hello, {guest_list[0]}, you are invited to a dinner!"
+print(invite1)
+invite2 = f"Hello, {guest_list[1]}, you are invited to a dinner!"
+print(invite2)
+invite3 = f"Hello, {guest_list[2]}, you are invited to a dinner!"
+print(invite3)
+invite4 = f"Hello, {guest_list[3]}, you are invited to a dinner!"
+print(invite4)
+invite5 = f"Hello, {guest_list[4]}, you are invited to a dinner!"
+print(invite5)
+invite6 = f"Hello, {guest_list[5]}, you are invited to a dinner!"
+print(invite6)
+invite7 = f"Hello, {guest_list[6]}, you are invited to a dinner!"
+print(invite7)
+print(guest_list)
+print(len(guest_list))
