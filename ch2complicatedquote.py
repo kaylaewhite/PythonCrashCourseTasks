5fb9c5f5be15ca985bd80192b6e057aca095dac5
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-17 18:43:02 -0400

    Add files via upload

---
+++ ch2complicatedquote.py
@@ -0,0 +1,4 @@
+famous_person = "Albert Einstein"
+quote = "\"It's not that I'm so smart, it's just that I stay with problems longer.\""
+message = f"{famous_person} once said, {quote}"
+print(message)
