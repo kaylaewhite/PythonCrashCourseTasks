8860e530a93537147374052a44eefac223cfd238
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-17 18:32:58 -0400

    Sorting exercises from Chapter 3

---
+++ ch3sorting.py
@@ -0,0 +1,10 @@
+locations = ['Venice', 'Iceland', 'California', 'Moscow', 'Thailand', 'France']
+print(locations)
+print(sorted(locations))
+print(sorted(locations, reverse=True))
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
